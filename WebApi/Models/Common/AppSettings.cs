b49341d8fb1e18df76feb3cab91f798bd693cb3b
Carlos Perales <carlosperales050@gmail.com>
2025-04-24 11:39:53 -0600

    agregando modelos

---
+++ WebApi/Models/Common/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApi.Models.Common
+{
+    public class AppSettings
+    {
+        public string Secreto { get; set; }
+    }
+}
---
+++ WebApi/Models/Response/UserResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models.Response
+{
+    public class UserResponse
+    {
+        public string Email { get; set; }
+
+        public string Token { get; set; }
+    }
+}
