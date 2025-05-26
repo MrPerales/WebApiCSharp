42548f4e33e28ec3feeca5dbda9ae44557f0d7d7
Carlos Perales <carlosperales050@gmail.com>
2025-05-26 16:34:14 -0600

    agregamos modelos de respuesta (venta)

---
+++ WebApi/Models/Request/ventaRequest.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApi.Models.Request
+{
+    public class VentaRequest
+    {
+        public int IdUser { get; set; }
+        public decimal Total { get; set; }
+        
+       public List<ConceptoRequest> Conceptos { get; set; }
+        public VentaRequest() {
+            //para que no nos agregen otra cosa inicializamos 
+            this.Conceptos = new List<ConceptoRequest>();
+        }
+    }
+
+    public class ConceptoRequest
+    {
+
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public decimal Importe { get; set; }
+
+        public int IdProducto{ get; set;}
+    }
+}
