9cb826d6326c6e7ea55734db2a1a88a2201f3504
Carlos Perales <carlosperales050@gmail.com>
2025-05-26 16:33:23 -0600

    agregamos dtos para el futuro

---
+++ WebApi/DTOs/Person/CreatePersonDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.DTOs.Person
+{
+    public class CreatePersonDto
+    {
+        public required string Name { get; set; }
+        public required int Age { get; set; }
+
+        public required string Email { get; set; }
+    }
+}
---
+++ WebApi/DTOs/Person/UpdatePersonDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.DTOs.Person
+{
+    public class UpdatePersonDto
+    {
+        public string? Name { get; set; }
+        public int? Age { get; set; }
+        public string? Email { get; set; }
+    }
+}
