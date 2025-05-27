13af00c386a56dffa500d1d3c700f35aa2c0ff42
Carlos Perales <carlosperales050@gmail.com>
2025-05-26 18:08:49 -0600

    agregamos transactions

--- WebApi/Controllers/VentaController.cs
+++ WebApi/Controllers/VentaController.cs
@@ -21,31 +21,41 @@ namespace WebApi.Controllers
         [HttpPost]
         public async Task<IActionResult> Add(VentaRequest model) {
 
-            var response = new Response<VentaRequest>(); 
-
+            var response = new Response<VentaRequest>();
+            //al usar transaction se bloquean las tablas que esten usandose en este caso "Venta ,conceptos"
+            //
+            var transaction = _context.Database.BeginTransaction();
             try
             {
-                var venta = new Venta();
-                venta.Total = model.Total;
-                venta.Fecha = DateTime.Now;
-                venta.IdUser = model.IdUser;
-                _context.Venta.Add(venta);
-               await _context.SaveChangesAsync();
+                //otro try/catch porque me hace un rollback mas facil y puedo ocupar hacer otras cosas 
+                try { 
+                    var venta = new Venta();
+                    //calculamos el total desde los conceptos , los cuales nos manda la request
+                    venta.Total = model.Conceptos.Sum(d=> d.Cantidad * d.PrecioUnitario);
+                    venta.Fecha = DateTime.Now;
+                    venta.IdUser = model.IdUser;
+                    _context.Venta.Add(venta);
+                    await _context.SaveChangesAsync();
 
-                //ya que son una lista de coceptos leemos cada propiedad y
-                //guardamos los datos de los conceptos 
-                foreach (var modelConcepto in model.Conceptos) {
-                    Console.WriteLine("en los conceptos");
-                    var concepto = new Concepto();
-                    concepto.Cantidad = modelConcepto.Cantidad;
-                    concepto.IdProducto = modelConcepto.IdProducto;
-                    concepto.PrecioUnitario= modelConcepto.PrecioUnitario;
-                    concepto.Importe = modelConcepto.Importe;
-                    concepto.IdVenta = venta.Id;
-                    _context.Conceptos.Add(concepto);
-                 await _context.SaveChangesAsync();
+                    //ya que son una lista de coceptos leemos cada propiedad y
+                    //guardamos los datos de los conceptos 
+                    foreach (var modelConcepto in model.Conceptos) {
+                        Console.WriteLine("en los conceptos");
+                        var concepto = new Concepto();
+                        concepto.Cantidad = modelConcepto.Cantidad;
+                        concepto.IdProducto = modelConcepto.IdProducto;
+                        concepto.PrecioUnitario= modelConcepto.PrecioUnitario;
+                        concepto.Importe = modelConcepto.Importe;
+                        concepto.IdVenta = venta.Id;
+                        _context.Conceptos.Add(concepto);
+                        await _context.SaveChangesAsync();
+                    }
+                    transaction.Commit(); //se utiliza para verificar que todo fue Ok y terminamos la transaction
+                    //nota : NO usar "transaction.Commit()" en finally porque entraria si se hace el rollback
+                    response.Success = true;
+                } catch (Exception) {
+                    transaction.Rollback(); //si algo fallo regresa en el tiempo hasta antes de hacer los cambios
                 }
-                response.Success = true;
             }
             catch (Exception ex)
             {
